d0c60860877a7b3f5d7b5ecc9874e4ffb0e9e3a2
bchen838 <bchen838@gmail.com>
2026-06-25 16:25:01 -0700

    Redirect root route to login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 import Register from './components/Register'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
 
 
@@ -10,6 +10,7 @@ function App() {
   return(
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to='/login' /> } />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={
